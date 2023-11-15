9f93d24973fa483e357469e108c3944a57101fb5
chart099 <122659980+chart099@users.noreply.github.com>
2023-11-14 21:27:43 -0600

    finished controllers

--- models/Thought.js
+++ models/Thought.js
@@ -38,20 +38,3 @@ thoughtSchema
 const Thought = model('thought', thoughtSchema);
 
 module.exports = Thought;
-
-// thoughtText
-// String
-// Required
-// Must be between 1 and 280 characters
-
-// createdAt
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-
-// username (The user that created this thought)
-// String
-// Required
-
-// reactions (These are like replies)
-// Array of nested documents created with the reactionSchema
