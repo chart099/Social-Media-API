173e99618fcbaad66cd0d8f23dded62a70a0c839
chart099 <122659980+chart099@users.noreply.github.com>
2023-11-18 21:41:15 -0600

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Social-Media-API
+
+This is a social media API that allows for many typical social media functions like: get all users, get a single user, add a user, update a user profile, and delete a user. Similar functions are used to manipulate posts or "thoughts" and reactions to these thoughts. 
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,16 +11,22 @@ const {
 
 } = require('../../controllers/thoughtController');
 
+// make a new thought
 router.route('/').get(getThoughts).post(createThought);
 
-router
-    .route('/:thoughtId')
-    .get(getSingleThought)
-    .put(updateThought)
-    .delete(deleteThought);
+// get single thought
+router.route('/:thoughtId').get(getSingleThought)
 
+// update a thought
+router.route('/:thoughtId').get(getSingleThought).put(updateThought)
+    
+// delete a thought
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+
+// add a reaction to a thought
 router.route('/:thoughtId/reactions').post(addThoughtReaction);
 
+// delete a reaction to a thought
 router.route('/:thoughtId/reactions/:reactionId').delete(removeThoughtReaction);
 
 module.exports = router;
